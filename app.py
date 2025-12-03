dfb9c1668d5f2902976ae9e861db88b23e5c9de2
pablaofficeal <pavlogorosko56@gmail.com>
2025-12-03 17:34:56 +0100

    start

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
+from config import Config
+from models import *
+from routers.home.main_home import home_bpp
 
 app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
+app.config.from_object(Config)
+db.init_app(app)
+app.register_blueprint(home_bpp)
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True)
