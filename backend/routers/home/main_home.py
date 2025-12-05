ba081b6567753aacccccaffb40a021aa18389b30
pablaofficeal <pavlogorosko56@gmail.com>
2025-12-05 15:46:25 +0100

    Добавлено управление пользователями: реализована проверка сессии для отображения приветствия и навигации. Обновлены шаблоны для отображения контента в зависимости от статуса пользователя. Добавлены новые стили для контекста и модальных окон.

--- backend/routers/home/main_home.py
+++ backend/routers/home/main_home.py
@@ -1,7 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session
+from models import *
 
 home_bpp = Blueprint('home_bpp', __name__)
 
 @home_bpp.route('/')
 def index():
-    return render_template('index.html')
+    user = None
+    if 'user_id' in session:
+        user = User.query.get(session['user_id'])
+    return render_template('index.html', user=user)
