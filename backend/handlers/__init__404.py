d8f1631c71a24ade391d735fefe011b81c927cdc
pablaofficeal <pavlogorosko56@gmail.com>
2025-12-07 23:19:28 +0100

    Добавлено логирование ошибок 404 с указанием URL и IP-адреса пользователя.

--- backend/handlers/__init__404.py
+++ backend/handlers/__init__404.py
@@ -1,4 +1,8 @@
-from flask import render_template
+from flask import render_template, request
+from utils.logs_service import init_logger
+
+logger = init_logger('error_handler')
 
 def not_found_error(error):
+    logger.warning(f"404 ERROR: {request.url} - IP: {request.remote_addr}")
     return render_template('errors/404.html'), 404
